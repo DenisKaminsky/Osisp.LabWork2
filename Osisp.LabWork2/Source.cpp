002fa406f869b501b19f24285a33b05091d31971
DenisKaminsky <deniskaminsky123@mail.ru>
2018-10-30 22:46:19 +0300

    add drawstring

--- Osisp.LabWork2/Source.cpp
+++ Osisp.LabWork2/Source.cpp
@@ -1,19 +1,28 @@
 #include <windows.h>
 #include <string>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib") 
 
 using namespace std;
+using namespace Gdiplus;
 
 #define SCROLL_SPEED 30
 #define MAX_ROWS_COUNT 30
 #define MAX_COLUMNS_COUNT 30
+#define MAX_LETTER_HEIGHT 35
+#define MIN_LETTER_HEIGHT 10
+#define MAX_LETTER_WIDTH 20
+#define MIN_LETTER_WIDTH 3
 
-int rows = 5;
-int columns = 30;
+ULONG_PTR gdiplusToken;
+int rows = 5,columns = 6;//количество строк и столбцов
+int top = 0, bottom = 0;//координаты Y для верха и ниха таблицы
+int letterWidth = 6, letterHeight = 20;//ширина и высота шрифта
+int textSpacing = 0; //межбуквенный интервал
+float epsilon = 0;//коэфициент пропорции
+string** matrix;//матрица строк
 
-int top = 0, bottom = 0;
-
-string** matrix;
-
+//генерирование матрицы
 void GenerateMatrix()
 {
 	int randcounter = 0;
@@ -30,15 +39,15 @@ void GenerateMatrix()
 		rows = MAX_ROWS_COUNT;
 
 	matrix = new string*[rows];
-
 	for (int i = 0; i < rows; i++)
 	{
 		matrix[i] = new string[columns];
 		for (int j = 0; j < columns; j++)
 		{
 			randcounter = rand() % 8 + 1;
+			s += to_string(randcounter);
 			for (int k= 0;k<randcounter;k++)
-				s+="sometext" + to_string(counter)+" ";
+				s+="someaeba" + to_string(counter)+" ";
 			matrix[i][j] = s;
 			s = "";
 			counter++;
@@ -46,6 +55,7 @@ void GenerateMatrix()
 	}
 }
 
+//удаление матрицы
 void RemoveMatrix()
 {
 	for (int i = 0; i < rows; i++)
@@ -54,10 +64,23 @@ void RemoveMatrix()
 	}
 }
 
-float GetRowSize(HDC hdc, int rowNumber,float hx)
+int GetTextLength(HDC hdc,int i,int j)
 {
-	int maxLength = 0;
-	int maxIndex = 0;
+	RECT rect;
+	rect.top = 0;
+	rect.left = 0;
+	rect.right = 1;
+	rect.bottom = 1;
+	DrawText(hdc, matrix[i][j].c_str(), matrix[i][j].length(), &rect, DT_CALCRECT);
+	return rect.right;
+}
+
+//получение максимальной высоты указанной строки
+float GetRowHeight(HDC hdc, int rowNumber,float hx,int borderSize)
+{
+	int maxLength = 0, maxIndex = 0;
+	RECT rect;
+
 	for (int i = 0; i < columns; i++)
 	{
 		int length = matrix[rowNumber][i].length();
@@ -67,50 +90,79 @@ float GetRowSize(HDC hdc, int rowNumber,float hx)
 			maxIndex = i;
 		}
 	}
-	RECT rect;
 	rect.top = 0;
 	rect.left = 0;
-	rect.right = hx-2;
-	rect.bottom = 1;
-	DrawText(hdc, matrix[rowNumber][maxIndex].c_str(), maxLength, &rect,DT_CALCRECT| DT_WORDBREAK| DT_EDITCONTROL | DT_CENTER);
-	int textRows = (int)((rect.right / (hx - 2)) + 1);
-	return (float)(textRows*rect.bottom+2);
+	rect.right = (long)hx-2*borderSize;
+	rect.bottom = borderSize;
+	DrawText(hdc, matrix[rowNumber][maxIndex].c_str(), maxLength, &rect,DT_CALCRECT| DT_WORDBREAK | DT_EDITCONTROL | DT_CENTER);
+	return (float)(rect.bottom+2*borderSize);
 }
 
-void DrawTable(HDC hdc,int sx,int sy)
+//рисование таблицы
+void DrawTable(HDC hdc,int sx,int sy,int borderSize)
 {
 	float posX = 0, posY = (float)top;
 	float hx = (float)sx / columns;
-	float hy = 0;
+	float hy = 0, extTextLineLength;
+	int extTextLength, linesCount;
+	HFONT hFont;
+	RECT rect;
+	SIZE sz;
+	Graphics *g = new Graphics(hdc);
+	Brush *brush = new SolidBrush(Color::RoyalBlue);
+	StringFormat *format = new StringFormat();
+	Font *font;
+
+	//letterWidth = (int)((hx - 2 * borderSize) / 10)+5;
+	//letterHeight = (int)(epsilon*letterWidth);
+
+	textSpacing = (int)((hx - 2 * borderSize)/5);	
+	hFont = CreateFont(letterHeight,letterWidth,0, 0, FW_DONTCARE, FALSE, FALSE,
+		FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH,
+		TEXT("Times New Roman"));
+	font = new Font(hdc, hFont);
+	SelectObject(hdc, hFont);
+	//SetTextCharacterExtra(hdc, textSpacing);
+	GetTextExtentPoint32(hdc, matrix[0][0].c_str(), matrix[0][0].length(), &sz);
+	extTextLength = sz.cx;
+	linesCount = (int)((GetTextLength(hdc,0,0)/(hx -2*borderSize)) + 1);
+	extTextLineLength = (float)extTextLength / linesCount;
+	if (extTextLineLength < hx)
+		extTextLineLength = hx;
 
 	for (int i = 0; i < rows; i++)
 	{
-		hy = GetRowSize(hdc,i, hx);
+		hy = GetRowHeight(hdc,i, hx,borderSize)*5;
 		posX = 0;
 		for (int j = 0; j < columns; j++)
 		{
-			MoveToEx(hdc, posX, posY, NULL);
-			LineTo(hdc, posX, posY+hy);
-			RECT rect;
-			rect.top = posY+1;
-			rect.left = posX + 1;
-			rect.right = posX + hx - 1;
-			rect.bottom = posY+hy - 1;
-			LPCSTR str = matrix[i][j].c_str();
-			DrawText(hdc, matrix[i][j].c_str(), matrix[i][j].length(), &rect, DT_WORDBREAK | DT_EDITCONTROL| DT_CENTER);
+			MoveToEx(hdc, (int)posX, (int)posY, NULL);
+			LineTo(hdc, (int)posX, (int)(posY+hy));
+			rect.top = (long)(posY+borderSize);
+			rect.left = (long)(posX + borderSize);
+			rect.right = (long)(posX + ((hx)*(hx)/ extTextLineLength) -borderSize);
+			rect.bottom = (long)(posY+hy - borderSize);			
+			DrawText(hdc, matrix[i][j].c_str(), matrix[i][j].length(), &rect, DT_WORDBREAK | DT_EDITCONTROL | DT_LEFT | DT_NOCLIP);
+			/*RectF *r = new RectF(rect.left, rect.top, hx, hy);
+			wstring ws = wstring(matrix[i][j].begin(), matrix[i][j].end());
+			const wchar_t* resstr = ws.c_str();
+			g->DrawString(resstr, matrix[i][j].length(), font, *r,format,brush);*/
 			posX += hx;
 		}
 		
-		MoveToEx(hdc, posX-1, posY, NULL);
-		LineTo(hdc, posX-1, posY + hy);
+		MoveToEx(hdc, (int)(posX-1), (int)posY, NULL);
+		LineTo(hdc, (int)(posX-1), (int)(posY + hy));
 
-		MoveToEx(hdc, 0, posY, NULL);
-		LineTo(hdc, sx, posY);
+		MoveToEx(hdc, 0, (int)posY, NULL);
+		LineTo(hdc, sx, (int)posY);
 		posY += hy;
 	}
-	MoveToEx(hdc, 0, posY, NULL);
-	LineTo(hdc, sx, posY);
-	bottom = posY;
+	MoveToEx(hdc, 0, (int)posY, NULL);
+	LineTo(hdc, sx, (int)posY);
+	bottom = (int)posY;
+
+	DeleteObject(hFont);
 }
 
 //обработчик сообщений
@@ -118,23 +170,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-
+	TEXTMETRIC tm;
 	int step = 0;
-	int wheelDelta;
-	static int width, height;
+	int wheelDelta, MB_RESULT;;
+	static int width = 0, height = 0;
 
 	switch (message)
 	{
+	case WM_CREATE:
+	{
+		hdc = GetDC(hWnd);
+		GetTextMetrics(hdc, &tm);
+		epsilon = (float)tm.tmHeight/tm.tmAveCharWidth;
+		ReleaseDC(hWnd, hdc);
+		Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+		GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+		break;
+	}
+	case WM_KEYDOWN: // Обработка нажатия клавиши
+		if (wParam == 68 || wParam == 39) //вправо
+			if (letterWidth < MAX_LETTER_WIDTH)
+				letterWidth++;
+		if (wParam == 65 || wParam == 37) //влево
+			if (letterWidth > MIN_LETTER_WIDTH)
+				letterWidth--;
+		if (wParam == 83 || wParam == 40) //вниз
+			if (letterHeight < MAX_LETTER_HEIGHT)
+				letterHeight++;
+		if (wParam == 87 || wParam == 38) //вверх
+			if (letterHeight > MIN_LETTER_HEIGHT)
+				letterHeight--;
+		if (wParam == 27) //если нажали ESC то выходим 
+		{
+			MB_RESULT = MessageBox(hWnd, "Вы действительно хотите выйти ?", "Выход", MB_YESNO);
+			if (MB_RESULT == 6)
+				SendMessage(hWnd, WM_DESTROY, wParam, lParam);
+		}
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
 	case WM_GETMINMAXINFO:
 	{
 		MINMAXINFO *pInfo = (MINMAXINFO *)lParam;
-		POINT Min = {columns*20, 500};
+		POINT Min = {columns*7, 500};
 		pInfo->ptMinTrackSize = Min;
 		break;
 	}
 	case WM_SIZE: 
 		width = LOWORD(lParam);
-		height = HIWORD(lParam);
+		height = HIWORD(lParam);		
 		ScrollWindow(hWnd, 0, -top, NULL, NULL);
 		UpdateWindow(hWnd);
 		top = 0;
@@ -169,12 +252,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	//обработка сообщений перерисовки
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);//дискриптор контекста устройства клиентской области окна
-		DrawTable(hdc, width, height);
+		DrawTable(hdc, width, height,3);
 		ReleaseDC(hWnd, hdc);
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
 		RemoveMatrix();
+		Gdiplus::GdiplusShutdown(gdiplusToken);
 		PostQuitMessage(0);
 		break;
 	default:
@@ -214,7 +298,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 	//отображение окна
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);	
-
 	//прием сообщений
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
